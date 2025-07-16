cb7eb0fa971f625d3882c73ed480b7372a4b9860
TomJourney <pacosongreen@gmail.com>
2025-07-17 06:58:45 +0800

    torch guide.

---
+++ src/chpater02/test0207_p51_torch_guide_main.py
@@ -0,0 +1,11 @@
+# 微积分
+# 解释导数
+import torch
+from torch.distributions import multinomial
+
+# 查找模块中所有函数与类
+print(dir(torch.distributions))
+# ['AbsTransform', 'AffineTransform', ...]
+
+# 查找特定函数和类的用法
+print(help(torch.ones))
